c31284005f49edfda471f9570ada471637e025bb
fabiobf96 <56910403+fabiobf96@users.noreply.github.com>
2021-10-06 00:02:59 +0200

    Add files via upload

---
+++ cocktaildb/src/application/SampleController.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.event.Event;
+import javafx.event.EventType;
+
+public class SampleController extends Event{
+
+	public SampleController(EventType<? extends Event> arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@XML
+	
+	
+	
+	
+}
